71fef91deb906d32f7a1a0b058ad80888788cdd6
Chibuzor Ezeufoh <Ezeufohchibuzor@gmail.com>
2024-05-05 16:13:58 +0100

    creating the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,16 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <div>
-        <h1>HELLO WORLD</h1>
+      <div className="home-container">
+        <div className="home-content">
+          <h1 className="home-title">Feedback Collection Page</h1>
+          <p className="home-description">
+            THis is where Students and Staff come to express either their
+            pleasure or displeasure with the services been rendered to them on
+            daily basis
+          </p>
+        </div>
+        <div className="home-image"></div>
       </div>
     </>
   );

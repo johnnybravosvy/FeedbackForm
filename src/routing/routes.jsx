aec0f6a209aba95837e6b706b880fc288a50946c
Chibuzor Ezeufoh <Ezeufohchibuzor@gmail.com>
2024-05-10 03:25:29 +0100

    added dashboard route

--- src/routing/routes.jsx
+++ src/routing/routes.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../components/Home";
 import Navbar from "../components/Navbar";
 import Login from "../components/Login";
+import Dashboard from "../components/Dashboard";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/navbar", element: <Navbar /> },
   { path: "/Login", element: <Login /> },
+  { path: "/dashboard", element: <Dashboard /> },
 ]);
 
 export default router;

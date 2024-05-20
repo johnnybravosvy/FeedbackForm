89a39ee234e2fafe98551d67721439357d95807f
Chibuzor Ezeufoh <Ezeufohchibuzor@gmail.com>
2024-05-20 18:23:59 +0100

    changes to login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,9 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const Login = ({ setRoleVar }) => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [role, setRole] = useState();
   const [loginData, setLoginData] = useState({
     username: "",
     password: "",
@@ -37,7 +40,6 @@ const Login = ({ setRoleVar }) => {
 
   return (
     <>
-      <Navbar />
       <form action="" className="login-page">
         <div className="login-container">
           <h2>Login</h2> <br />
@@ -84,3 +86,89 @@ const Login = ({ setRoleVar }) => {
 };
 
 export default Login;
+
+// import { useState } from "react";
+// import Navbar from "./Navbar";
+// import axios from "axios";
+// import { useNavigate } from "react-router-dom";
+
+// const Login = ({ setRoleVar }) => {
+//   const [loginData, setLoginData] = useState({
+//     username: "",
+//     password: "",
+//     role: "",
+//   });
+//   const navigate = useNavigate();
+
+//   function handleChange(event) {
+//     const { name, value } = event.target;
+//     setLoginData((prevLoginData) => {
+//       return {
+//         ...prevLoginData,
+//         [name]: value,
+//       };
+//     });
+//   }
+
+//   axios.defaults.withCredentials = true;
+//   function handleSubmit() {
+//     axios
+//       .post("http://localhost:5000/auth/login", loginData)
+//       .then((res) => {
+//         if (res.data.login && res.data.role === "admin") {
+//           navigate("/dashboard");
+//         } else if (res.data.login && res.data.role === "user") {
+//           navigate("/");
+//         }
+//       })
+//       .catch((err) => console.log(err));
+//   }
+
+//   return (
+//     <>
+//       <form action="" className="login-page">
+//         <div className="login-container">
+//           <h2>Login</h2> <br />
+//           <div className="form-group">
+//             <label htmlFor="username">Username:</label>
+//             <input
+//               type="text"
+//               placeholder="Enter Username"
+//               onChange={handleChange}
+//               name="username"
+//               value={loginData.username}
+//             />
+//           </div>
+//           <div className="form-group">
+//             <label htmlFor="password">Password:</label>
+//             <input
+//               type="password"
+//               placeholder="enter password"
+//               onChange={handleChange}
+//               name="password"
+//               value={loginData.password}
+//             />
+//           </div>
+//           <div className="form-group">
+//             <label htmlFor="role">Role:</label>
+//             <select
+//               name="role"
+//               id="role"
+//               onChange={handleChange}
+//               value={loginData.role}
+//             >
+//               <option value="">--Choose--</option>
+//               <option value="admin">Admin</option>
+//               <option value="user">User</option>
+//             </select>
+//           </div>
+//           <button className="btn-login" onSubmit={handleSubmit}>
+//             Login
+//           </button>
+//         </div>
+//       </form>
+//     </>
+//   );
+// };
+
+// export default Login;
